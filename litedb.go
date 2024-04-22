8aeeb9bf50bbba1b5a593c9c2bd8277ef0b0bf50
endernan4u <adryanender@gmail.com>
2024-04-22 13:14:44 +0200

    Added tests for json

--- litedb.go
+++ litedb.go
@@ -2,13 +2,14 @@ package litedb
 
 import (
 	"os"
+	"path/filepath"
 )
 
 type LiteDb struct {
 	dir_path string
 }
 
-func (db *LiteDb) open() (string, error) {
-	data, err := os.ReadFile(db.dir_path)
-	return string(data), err
+func (db *LiteDb) Open(file_name string) ([]byte, error) {
+	data, err := os.ReadFile(filepath.Join(db.dir_path, file_name))
+	return data, err
 }
--- litedb_test.go
+++ litedb_test.go
@@ -1,16 +1,43 @@
 package litedb
 
 import (
+	"encoding/json"
 	"testing"
 )
 
 func TestMain(t *testing.T) {
-	db := LiteDb{dir_path: "./test_db/test.txt"}
-	data, err := db.open()
+	db := LiteDb{dir_path: "./test_db"}
+	data, err := db.Open("test.json")
+
+	var buffor map[string]interface{}
+	json.Unmarshal(data, &buffor)
+
 	if err != nil {
 		t.Error("Error reading")
 	}
-	if data != "data=data" {
-		t.Errorf("Data doesn't match: %s", data)
+
+	if buffor["data"] != "data" {
+		t.Errorf("Error")
 	}
+}
+
+type res struct {
+	Data []string `json:"data"`
+}
+
+func TestArray(t *testing.T) {
+	db := LiteDb{dir_path: "./test_db"}
+	data, err := db.Open("array.json")
+
+	buffor := res{}
+	json.Unmarshal(data, &buffor)
+
+	if err != nil {
+		t.Error("Error reading")
+	}
+
+	if buffor.Data[0] != "gg" {
+		t.Errorf("Data is wrong. \n Data: %s", buffor)
+	}
+
 }
