b2e001cadab6d85a56d2e1734ec55b1f8643351d
endernan4u <adryanender@gmail.com>
2024-04-22 11:08:37 +0200

    Added reading

--- litedb.go
+++ litedb.go
@@ -1,11 +1,14 @@
 package litedb
 
-import "fmt"
+import (
+	"os"
+)
 
 type LiteDb struct {
 	dir_path string
 }
 
-func (db *LiteDb) open() {
-	fmt.Println("opened")
+func (db *LiteDb) open() (string, error) {
+	data, err := os.ReadFile(db.dir_path)
+	return string(data), err
 }
--- litedb_test.go
+++ litedb_test.go
@@ -1,8 +1,16 @@
 package litedb
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMain(t *testing.T) {
-	db := LiteDb{dir_path: "./dbs"}
-	db.open()
+	db := LiteDb{dir_path: "./test_db/test.txt"}
+	data, err := db.open()
+	if err != nil {
+		t.Error("Error reading")
+	}
+	if data != "data=data" {
+		t.Errorf("Data doesn't match: %s", data)
+	}
 }
