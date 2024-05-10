96ae92c8b67c12018a8a0b0410682218ba34d424
endernan4u <adryanender@gmail.com>
2024-05-10 16:15:25 +0200

    Updated cache file Added cache data and update

--- cache.go
+++ cache.go
@@ -6,7 +6,7 @@ import (
 	"path/filepath"
 )
 
-type cache struct {
+type Cache struct {
 	Last_Index int    `json:"last_index"`
 	Table_Name string `json:"table_name"`
 }
@@ -15,19 +15,35 @@ const (
 	cache_file_name = ".cache"
 )
 
-func contCacheFile(dir_path string) bool {
-	_, err := os.Stat(filepath.Join(dir_path, cache_file_name))
-	return err != nil
+func contCacheFile(table_path string) bool {
+	_, err := os.Stat(filepath.Join(table_path, cache_file_name))
+	return err == nil
 }
 
-func createCacheFile(dir_path string, tab_name string) {
-	cache_path := filepath.Join(dir_path, cache_file_name)
-	default_cache := cache{
+func createCacheFile(table_path string, tab_name string) {
+	cache_path := filepath.Join(table_path, cache_file_name)
+
+	default_cache := Cache{
 		Last_Index: 0,
 		Table_Name: tab_name,
 	}
 
 	json_cache, _ := json.Marshal(default_cache)
+
+	SaveFile(cache_path, json_cache)
+}
+
+func cacheData(tab table) Cache {
+	cache_path := filepath.Join(tab.path, cache_file_name)
+
+	cache_data, _ := OpenFile(cache_path)
+
+	return BytesToJson[Cache](cache_data)
+}
+
+func cacheUpdate(tab table, data Cache) {
+	cache_path := filepath.Join(tab.path, cache_file_name)
+	json_cache, _ := json.Marshal(data)
 
 	SaveFile(cache_path, json_cache)
 }
